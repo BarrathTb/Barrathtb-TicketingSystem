1e4efc38cbcd45f2eb38105376eed87800d6a1b9
barrathtb <thomas.barrath@lanex.com>
2023-08-29 14:25:17 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+        // Get the input from the user
+        Console.WriteLine("Enter the Ticket ID:");
+        int ticketId = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the Ticket Summary:");
+        string summary = Console.ReadLine();
+
+        Console.WriteLine("Enter the Ticket Status:");
+        string status = Console.ReadLine();
+
+        Console.WriteLine("Enter the Ticket Priority:");
+        string priority = Console.ReadLine();
+
+        Console.WriteLine("Enter the Ticket Submitter:");
+        string submitter = Console.ReadLine();
+
+        Console.WriteLine("Enter the Ticket Assigned:");
+        string assigned = Console.ReadLine();
+
+        Console.WriteLine("Enter the Ticket Watching (separated by '|'):");
+        string watching = Console.ReadLine();
+
+        // Create a new record in the CSV file
+        string record = $"{ticketId},{summary},{status},{priority},{submitter},{assigned},\"{watching}\"";
+
+        // Append the new record to the existing CSV file
+        File.AppendAllText("Tickets.csv", Environment.NewLine + record);
+
+        Console.WriteLine("Ticket added successfully!");
+
