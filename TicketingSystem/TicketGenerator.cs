ab65fd91de5a2f26ef9d7238b61f2051628c2d39
thomasbarrath <thomas.barrath@lanex.com>
2023-10-24 01:05:32 -0500

    Create ticket generator

--- TicketingSystem/TicketGenerator.cs
+++ TicketingSystem/TicketGenerator.cs
@@ -1,41 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 public class TicketGenerator
 {
-    public string ticketHeader = "TicketId: | Summary: | Status: | Priority: | Submitter: | Assigned: | Watching: ";
-    public string TicketHeader => ticketHeader;
-
     public string GenerateTicketList(List<Ticket> ticketList)
     {
-        var ticketLineItem = "";
+        var ticketLineItem = new StringBuilder();
 
         foreach (var ticket in ticketList)
         {
-            ticketLineItem += $"{ticket.TicketId} | {ticket.Summary} | {ticket.Status} | {ticket.Priority} | {ticket.Submitter} | {ticket.Assigned} | ";
-
-            foreach (var watcher in ticket.Watching)
+            if (ticket is BugDefectTicket bugDefect)
             {
-                ticketLineItem += $"{watcher} | ";
+                // Generate lines for BugDefectTicket
+                ticketLineItem.Append($"{bugDefect.TicketId},{bugDefect.Summary},{bugDefect.Status},{bugDefect.Priority},{bugDefect.Submitter},{bugDefect.Assigned},{string.Join("|", bugDefect.Watching)},{bugDefect.Severity}");
             }
-            ticketLineItem = $"{ticketLineItem[..^1]}\n";
-        }
-        return ticketLineItem;
-    }
-
-    public List<Ticket> GenerateTicket(List<string> ticketLines)
-    {
-        var ticketList = new List<Ticket>();
-        var ticketArray = ticketLines.ToArray();
-
-        foreach (var ticketLine in ticketArray)
-        {
-            if (ticketArray.Length >= 1)
+            else if (ticket is EnhancementTicket enhancement)
             {
-                var ticketParts = ticketLine.Split(',');
-                var newTicket = new Ticket(Convert.ToInt16(ticketParts[0]), ticketParts[1], ticketParts[2], ticketParts[3], ticketParts[4], ticketParts[5], ticketParts[6].Split("|"));
-
-                ticketList.Add(newTicket);
+                // Generate lines for EnhancementTicket
+                ticketLineItem.Append($"{enhancement.TicketId},{enhancement.Summary},{enhancement.Status},{enhancement.Priority},{enhancement.Submitter},{enhancement.Assigned},{string.Join("|", enhancement.Watching)},{enhancement.Software},{enhancement.Cost},{enhancement.Reason},{enhancement.Estimate}");
+            }
+            else if (ticket is TaskTicket task)
+            {
+                // Generate lines for TaskTicket
+                ticketLineItem.Append($"{task.TicketId},{task.Summary},{task.Status},{task.Priority},{task.Submitter},{task.Assigned},{string.Join("|", task.Watching)},{task.ProjectName},{task.DueDate}");
             }
 
+            ticketLineItem.Append("\n");
         }
-        return ticketList;
+
+        return ticketLineItem.ToString();
     }
 }
+
